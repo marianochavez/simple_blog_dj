3e78b5906880d3b009217494ef014335c45bded9
Mariano <amarianochavez@gmail.com>
2021-03-21 11:42:50 -0300

    add allowed_host

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -1,9 +1,9 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoblogch.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
